4badf3d2aa584e9da4a028f3487a671c43c08376
faruqmaulana <faruqchodot21@gmail.com>
2022-03-15 21:35:56 +0700

    refactoring code 🌱

--- pages/api/category/[...slug].js
+++ pages/api/category/[...slug].js
@@ -1,7 +1,6 @@
 import middleware from "../middleware/middleware";
-import { scrapeSite } from "../utils/utils";
-import { getDynamicCategory } from "../utils/get-category-data";
-import { cors, turndownService, VERCEL_BASE_URL } from "../utils/const";
+import { getDynamicCategory } from "../utils/get-dynamic-category";
+import { cors } from "../utils/const";
 
 export default async function handler(req, res) {
   await middleware(req, res, cors);
@@ -9,69 +8,12 @@ export default async function handler(req, res) {
   //get query params
   const { slug } = req.query;
   if (slug.length > 2) slug.splice(2);
+  const REQUEST_URL = slug.join("/");
 
   //make request
-  const REQUEST_URL = slug.join("/");
-  const { $, status } = await scrapeSite(REQUEST_URL);
+  const { status, featured_post, posts } = await getDynamicCategory(
+    REQUEST_URL
+  );
 
-  if (REQUEST_URL === "hashtag/Commentary" || REQUEST_URL === "most-viewed") {
-    const { status, posts } = await getDynamicCategory(REQUEST_URL);
-    return res.json({ status, featured_post: null, posts });
-  } else {
-    const link =
-      VERCEL_BASE_URL +
-      "/detailpost" +
-      $(".bigHeadline .descNews a").next().attr("href").replace(".html", "");
-
-    const image = $("figure img").attr("data-src");
-    const image_desc = $("figcaption").text().trim();
-    const headline = $(".descNews p").first().text().trim();
-    const title = $(".descNews h2.titleNews").first().text().trim();
-    const category = $(".descNews span.dt-news").first().text().trim();
-    const pusblised_at = $(".bigHeadline .descNews span").last().text().trim();
-    const featured_post = {
-      link,
-      title,
-      image,
-      headline,
-      category,
-      image_desc,
-      pusblised_at,
-    };
-
-    const posts = [];
-    $(".tjp-newsListing .listNews").each((i, el) => {
-      const badge = $(el)
-        .find("a span.premiumBadge span")
-        .text()
-        .replace(/^\s+|\s+$/gm, "");
-
-      const headline = turndownService.turndown(
-        $(el).find(".latestDetail p").html()
-      );
-
-      const link =
-        VERCEL_BASE_URL +
-        "/detailpost" +
-        $(el).find(".imageLatest a").attr("href").replace(".html", "");
-
-      const category = $(el).find(".dt-news").text();
-      const title = $(el).find(".titleNews").text().trim();
-      const premium_badge = badge === "" ? "not premium" : badge;
-      const image = $(".imageLatest", el).find("img").attr("data-src");
-      const pusblised_at = $(el).find(".latestDetail span.date").last().text();
-
-      posts.push({
-        link,
-        title,
-        image,
-        headline,
-        category,
-        pusblised_at,
-        premium_badge,
-      });
-    });
-
-    return res.json({ status, featured_post, posts });
-  }
+  return res.json({ status, featured_post, posts });
 }
---
+++ pages/api/utils/get-dynamic-category.js
@@ -0,0 +1,108 @@
+import { scrapeSite } from "./utils";
+import { turndownService, BASE_URL, VERCEL_BASE_URL } from "./const";
+
+export const getDynamicCategory = async (endpoint) => {
+  const { $, status } = await scrapeSite(endpoint);
+  if (endpoint === "hashtag/Commentary" || endpoint === "most-viewed") {
+    const posts = [];
+    $(".bgSingle .listNews").each((i, el) => {
+      const getLink = $(el)
+        .find(".imageNews")
+        .find("a")
+        .attr("href")
+        .replace(".html", "");
+
+      const link =
+        endpoint === "hashtag/Commentary"
+          ? getLink.replace(BASE_URL, VERCEL_BASE_URL + "/detailpost/")
+          : VERCEL_BASE_URL + "/detailpost/" + getLink;
+
+      const pusblised_at = $(el)
+        .find("span.date")
+        .clone()
+        .children()
+        .remove()
+        .end()
+        .text()
+        .trim();
+
+      const category = $(el).find("span span").text();
+      const title = $(el)
+        .find(".titleNews")
+        .clone()
+        .children()
+        .remove()
+        .end()
+        .text()
+        .trim();
+
+      const image = $(el).find(".imageNews").find("img").attr("data-src");
+
+      posts.push({
+        link,
+        title,
+        image,
+        category,
+        pusblised_at,
+      });
+    });
+
+    return { status, featured_post: null, posts };
+  } else {
+    const link =
+      VERCEL_BASE_URL +
+      "/detailpost" +
+      $(".bigHeadline .descNews a").next().attr("href").replace(".html", "");
+
+    const image = $("figure img").attr("data-src");
+    const image_desc = $("figcaption").text().trim();
+    const headline = $(".descNews p").first().text().trim();
+    const title = $(".descNews h2.titleNews").first().text().trim();
+    const category = $(".descNews span.dt-news").first().text().trim();
+    const pusblised_at = $(".bigHeadline .descNews span").last().text().trim();
+    const featured_post = {
+      link,
+      title,
+      image,
+      headline,
+      category,
+      image_desc,
+      pusblised_at,
+    };
+
+    const posts = [];
+    $(".tjp-newsListing .listNews").each((i, el) => {
+      const badge = $(el)
+        .find("a span.premiumBadge span")
+        .text()
+        .replace(/^\s+|\s+$/gm, "");
+
+      const headline = turndownService.turndown(
+        $(el).find(".latestDetail p").html()
+      );
+
+      const link =
+        VERCEL_BASE_URL +
+        "/detailpost" +
+        $(el).find(".imageLatest a").attr("href").replace(".html", "");
+
+      const category = $(el).find(".dt-news").text();
+      const title = $(el).find(".titleNews").text().trim();
+      const premium_badge = badge === "" ? "not premium" : badge;
+      const image = $(".imageLatest", el).find("img").attr("data-src");
+      const pusblised_at = $(el).find(".latestDetail span.date").last().text();
+
+      posts.push({
+        link,
+        title,
+        image,
+        headline,
+        category,
+        pusblised_at,
+        premium_badge,
+      });
+    });
+
+    return { status, featured_post, posts };
+  }
+};
