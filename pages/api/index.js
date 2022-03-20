6b0e67a3d86122d0d65ffbb683972ffde46da581
faruqmaulana <faruqchodot21@gmail.com>
2022-03-20 11:11:00 +0700

    change index res json

--- pages/api/index.js
+++ pages/api/index.js
@@ -5,6 +5,8 @@ export default function index(req, res) {
     return res.json({
       message: "Selamat Datang di JAKARTA POST API",
       "projects source": "https://github.com/faruqmaulana/JAKARTA-POST-API",
+      "dokumentasi API":
+        "https://github.com/faruqmaulana/JAKARTA-POST-API/blob/master/README.md",
       "mulai project anda dan jadilah produktif":
         "https://jakpost.vercel.app/api/category",
     });
