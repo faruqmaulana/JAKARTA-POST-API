8e83bc9e8645d7e91538a9f3fde8f266bf454e76
faruqmaulana <faruqchodot21@gmail.com>
2022-03-23 12:50:44 +0700

    change API format

--- pages/api/category.js
+++ pages/api/category.js
@@ -37,39 +37,39 @@ export default async function handler(req, res) {
       const name = getName === "MULTIMEDIA" ? "FRONT ROW" : getName;
 
       //get sub category
-      let categories = [];
+      let subCategory = [];
       $("li.tjp-li-" + iterate)
         .children()
         .last()
         .children()
         .find("a")
         .each((i, el) => {
-          const sub_category = $(el).text();
+          const name = $(el).text();
           const getSubLink = $(el)
             .attr("href")
             .replace(BASE_URL_SLUG, VERCEL_BASE_URL + "/category");
 
-          const sub_link =
+          const link =
             getSubLink === VERCEL_BASE_URL + "/category/multimedia/podcast"
               ? VERCEL_BASE_URL + "/podcast"
               : getSubLink;
 
-          return categories.push({
-            sub_link,
-            sub_category,
+          return subCategory.push({
+            name,
+            link,
           });
         });
 
       category.push({
-        link,
         name,
-        categories: categories.length !== 0 ? categories : null,
+        link,
+        subCategory: subCategory.length !== 0 ? subCategory : null,
       });
     }
     // delete "most-shared" category, because "most-shared" category contains nothing
     category.splice(1, 1);
     // delete "Longform Biz" sub-category, because "Longform Biz" sub-category does not contains news or podcasts
-    category[7].categories.splice(2, 1);
+    category[7].subCategory.splice(2, 1);
 
     return res.json({ status, category });
   } catch {
