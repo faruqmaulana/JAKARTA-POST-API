10565001abc42d4b5c1d9b2cf26da795614dc4bb
faruqmaulana <faruqchodot21@gmail.com>
2022-03-30 16:02:04 +0700

    delete unnecessary chrome aws lamda function

--- pages/api/utils/utils.js
+++ pages/api/utils/utils.js
@@ -3,7 +3,6 @@ import puppeteer from "puppeteer";
 import cheerio from "cheerio";
 import axios from "axios";
 import chrome from "chrome-aws-lambda";
-// const chromium = require("chrome-aws-lambda");
 
 export const scrapeSite = async (endpoint) => {
   try {
@@ -35,41 +34,3 @@ export function checkEmptyObj(obj) {
   for (var i in obj) return false;
   return true;
 }
-
-// let browserWSEndpoint;
-
-// export const puppeteerOpenBrowser = async function (event) {
-//   let browser, page;
-
-//   try {
-//     if (browserWSEndpoint) {
-//       browser = await chromium.puppeteer.connect({ browserWSEndpoint });
-//     }
-
-//     if (!browser || !browser.isConnected()) {
-//       browser = await chromium.puppeteer.launch({
-//         args: chromium.args,
-//         defaultViewport: chromium.defaultViewport,
-//         executablePath: await chromium.executablePath,
-//         headless: chromium.headless,
-//       });
-
-//       // Keep blank page open
-//       browser.newPage();
-//       await page.goto(`${BASE_URL}${endpoint}`, {
-//         waitUntil: "load",
-//         timeout: 0,
-//       });
-//       const content = await page.content();
-//       const $ = cheerio.load(content);
-//       browserWSEndpoint = browser.wsEndpoint();
-//     }
-
-//     page = browser.newPage();
-//     // ...
-//   } finally {
-//     if (page) page.close();
-//     if (browser) browser.disconnect();
-//     return { $ };
-//   }
-// };
