60a5d7c8cbb9849d5edbf3133dcd6801566ac60c
faruqmaulana <faruqchodot21@gmail.com>
2022-04-02 14:25:20 +0700

    trying fix error

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,77 +1,15 @@
-import runMiddleware from "./middleware/middleware";
-import {
-  cors,
-  BASE_URL_SLUG,
-  ERROR_MESSAGE,
-  VERCEL_BASE_URL,
-} from "./utils/const";
-import { scrapeSite } from "./utils/utils";
+import { ERROR_MESSAGE } from "./utils/const";
 
-export default async function handler(req, res) {
-  await runMiddleware(req, res, cors);
-
+export default function index(req, res) {
   try {
-    // make request
-    const { $, status } = await scrapeSite("");
-
-    // get category
-    let category = [];
-    for (let iterate = 4; iterate <= 13; iterate++) {
-      const getLink = $("li.tjp-li-" + iterate)
-        .children()
-        .first()
-        .attr("href")
-        .replace(BASE_URL_SLUG, "");
-
-      const link =
-        getLink === "/multimedia"
-          ? VERCEL_BASE_URL + "/category/front-row"
-          : VERCEL_BASE_URL + "/category" + getLink;
-
-      const getName = $("li.tjp-li-" + iterate)
-        .children()
-        .first()
-        .text()
-        .replace(/^\s+|\s+$/gm, "");
-
-      const name = getName === "MULTIMEDIA" ? "FRONT ROW" : getName;
-
-      //get sub category
-      let subCategory = [];
-      $("li.tjp-li-" + iterate)
-        .children()
-        .last()
-        .children()
-        .find("a")
-        .each((i, el) => {
-          const name = $(el).text();
-          const getSubLink = $(el)
-            .attr("href")
-            .replace(BASE_URL_SLUG, VERCEL_BASE_URL + "/category");
-
-          const link =
-            getSubLink === VERCEL_BASE_URL + "/category/multimedia/podcast"
-              ? VERCEL_BASE_URL + "/podcast"
-              : getSubLink;
-
-          return subCategory.push({
-            name,
-            link,
-          });
-        });
-
-      category.push({
-        name,
-        link,
-        subCategory: subCategory.length !== 0 ? subCategory : null,
-      });
-    }
-    // delete "most-shared" category, because "most-shared" category contains nothing
-    category.splice(1, 1);
-    // delete "Longform Biz" sub-category, because "Longform Biz" sub-category does not contains news or podcasts
-    category[7].subCategory.splice(2, 1);
-
-    return res.json({ status, category });
+    return res.json({
+      message: "Selamat Datang di JAKARTA POST API",
+      "projects source": "https://github.com/faruqmaulana/JAKARTA-POST-API",
+      "dokumentasi API":
+        "https://github.com/faruqmaulana/JAKARTA-POST-API/blob/master/README.md",
+      "mulai project anda dan jadilah produktif":
+        "https://jakpost.vercel.app/api/category",
+    });
   } catch {
     res.status(404).json({ status: 404, error: ERROR_MESSAGE });
   }
