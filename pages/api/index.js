fc158c9b88f4a343bd204304bd33b79c4fddd68a
faruqmaulana <faruqchodot21@gmail.com>
2022-04-02 13:53:01 +0700

    fix index res json

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,16 +1,10 @@
-import { ERROR_MESSAGE } from "./utils/const";
-
 export default function index(req, res) {
-  try {
-    return res.json({
-      message: "Selamat Datang di JAKARTA POST API",
-      "projects source": "https://github.com/faruqmaulana/JAKARTA-POST-API",
-      "dokumentasi API":
-        "https://github.com/faruqmaulana/JAKARTA-POST-API/blob/master/README.md",
-      "mulai project anda dan jadilah produktif":
-        "https://jakpost.vercel.app/api/category",
-    });
-  } catch {
-    res.status(404).json({ status: 404, error: ERROR_MESSAGE });
-  }
+  return res.json({
+    message: "Selamat Datang di JAKARTA POST API",
+    "projects source": "https://github.com/faruqmaulana/JAKARTA-POST-API",
+    "dokumentasi API":
+      "https://github.com/faruqmaulana/JAKARTA-POST-API/blob/master/README.md",
+    "mulai project anda dan jadilah produktif":
+      "https://jakpost.vercel.app/api/category",
+  });
 }
